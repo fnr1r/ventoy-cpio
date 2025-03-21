b716caec16dbcd285a26f7a75ad66fcf0b3af07e
fnrir <stepien241@gmail.com>
2025-03-21 10:13:42 +0100

    feat: proper unsquashfs build

--- tools/squashfs/build.sh
+++ tools/squashfs/build.sh
@@ -8,16 +8,18 @@ HERE="$(dirname "$(readlink -f -- "$0")")"
 . "$SCRIPTS_DIR/tool_build.sh"
 
 ARCH=""
-DIST_DIR=""
 CC=""
 CFLAGS=""
+CONFIGURE_OPTS=()
 MAKEBIN=()
 MAKEOPTS=()
+LDFLAGS=""
 STRIP_CMD=()
+Q_CFLAGS=""
+Q_LDFLAGS=""
 
 argparse() {
     ARCH="$1"
-    DIST_DIR="$HERE/dist/$ARCH"
     MAKEBIN=("${@:2}")
     MAKEOPTS=(
         GZIP_SUPPORT=1
@@ -27,88 +29,123 @@ argparse() {
         ZSTD_SUPPORT=1
         LZMA_XZ_SUPPORT=1
     )
-    CFLAGS="-Os -flto"
-    LDFLAGS="-static"
+    Q_CFLAGS="-Os -flto"
+    Q_LDFLAGS="-static"
 
     case $ARCH in
         aarch64)
-            exit 69
-            #CC="aarch64-linux-gnu-gcc"
-            #STRIP_CMD=(aarch64-linux-gnu-strip)
+            CC="aarch64-linux-gnu-gcc"
+            CONFIGURE_OPTS=(--host=arm-linux)
+            STRIP_CMD=(aarch64-linux-gnu-strip)
             ;;
         i386)
-            CFLAGS+=" -m32"
+            CC="gcc"
+            CFLAGS="-m32"
+            CONFIGURE_OPTS=(--host=i386-linux)
+            LDFLAGS="-m32"
             STRIP_CMD=(strip)
             ;;
         mips64el)
-            exit 69
-            #CC="mips64el-linux-musl-gcc"
-            #STRIP_CMD=(mips64el-linux-musl-strip)
-            #MAKEOPTS+=(
-            #    "CC=$CC"
-            #)
+            CC="mips64el-linux-musl-gcc"
+            CONFIGURE_OPTS=(--host=mips64el-linux)
+            STRIP_CMD=(mips64el-linux-musl-strip)
             ;;
         x86_64)
-            MAKEOPTS+=(
-                #"CC=musl-gcc"
-            )
+            CC="musl-gcc"
             STRIP_CMD=(strip --strip-all)
             ;;
         *)
             exit 69
             ;;
     esac
-    MAKEOPTS+=(
-        "CFLAGS=$CFLAGS"
-        "LDFLAGS=$LDFLAGS"
-    )
+
+    #BUILD_SUB_DIR="$ARCH"
+    TARGET_SUB_DIR="$ARCH"
 }
 
 copysrc() {
-    cp -ar src/. "$TMP_DIR"
+    cp -ar --reflink=auto src/. "$WORK_DIR"
 }
 
 TARGET_BIN="unsquashfs"
 
 build() {
-    PREFIX="$(pwd)/prefix"
-    MAKEOPTS+=(
-        #"CFLAGS="
-        "INCLUDEDIR+=$PREFIX/include"
-        "LDFLAGS+=$PREFIX/lib"
-    )
+    PREFIX="$WORK_DIR/prefix"
 
     pushd lz4 > /dev/null
-    "${MAKEBIN[@]}" -C lib "PREFIX=$PREFIX" install
+    CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" "${MAKEBIN[@]}" "CC=$CC" -C lib "PREFIX=$PREFIX" "BUILD_SHARED=no" install
     popd > /dev/null
 
     pushd lzo > /dev/null
-    ./configure "--prefix=$PREFIX"
+    #if ! [[ -f .configured ]]; then
+        LZO_CFLAGS=""
+        if [[ -n "$CFLAGS" ]]; then
+            LZO_CFLAGS+=" $CFLAGS"
+        fi
+        ./configure \
+            --prefix="$PREFIX" \
+            "${CONFIGURE_OPTS[@]}" "CC=$CC" CFLAGS="$LZO_CFLAGS"
+        #touch .configured
+    #fi
     "${MAKEBIN[@]}" install
     popd > /dev/null
 
     pushd xz > /dev/null
+    #if ! [[ -f .configured ]]; then
+        XZ_CFLAGS=""
+        if [[ -n "$CFLAGS" ]]; then
+            XZ_CFLAGS+=" $CFLAGS"
+        fi
+        ./configure \
+            --prefix="$PREFIX" \
+            --enable-shared=no --enable-static=yes \
+            --disable-xz --disable-xzdec \
+            --disable-lzmadec --disable-lzmainfo --disable-lzma-links \
+            --disable-scripts \
+            --disable-assembler \
+            "${CONFIGURE_OPTS[@]}" "CC=$CC" CFLAGS="$XZ_CFLAGS"
+        #touch .configured
+    #fi
+    "${MAKEBIN[@]}" install
+    popd > /dev/null
+
+    pushd zlib > /dev/null
+    #if ! [[ -f .configured ]]; then
+        ZLIB_CFLAGS="-O3"
+        if [[ -n "$CFLAGS" ]]; then
+            ZLIB_CFLAGS=" $CFLAGS"
+        fi
+        CC="$CC" CFLAGS="$ZLIB_CFLAGS" ./configure \
+            --prefix="$PREFIX" \
+            --static
+        #touch .configured
+    #fi
+    "${MAKEBIN[@]}" install
     popd > /dev/null
 
     pushd zstd > /dev/null
-    "${MAKEBIN[@]}" -C lib "PREFIX=$PREFIX" install
+    CFLAGS="$CFLAGS $Q_CFLAGS" LDFLAGS="$LDFLAGS $Q_LDFLAGS" "${MAKEBIN[@]}" \
+        "CC=$CC" "PREFIX=$PREFIX" -C lib \
+        install-static install-includes
     popd > /dev/null
 
     #pushd "squashfs-tools" > /dev/null
     cd squashfs-tools
     #MAKE=("${MAKEBIN[@]}" "${MAKEOPTS[@]}")
-    env "${MAKEOPTS[@]}" "${MAKEBIN[@]}" \
+    CFLAGS="$CFLAGS -I$PREFIX/include" LDFLAGS="$LDFLAGS -L$PREFIX/lib" "${MAKEBIN[@]}" \
+        "CC=$CC" "${MAKEOPTS[@]}" \
         "$TARGET_BIN"
     #popd > /dev/null
 }
 
 package() {
-    mkdir -p "$DIST_DIR"
-    cp -ar "$TARGET_BIN" "$DIST_DIR/$TARGET_BIN.debug"
+    cp -ar "$TARGET_BIN" "$TARGET_DIR/$TARGET_BIN.debug"
     "${STRIP_CMD[@]}" "$TARGET_BIN"
-    cp -ar "$TARGET_BIN" "$DIST_DIR/$TARGET_BIN.stripped"
-    upx "$TARGET_BIN"
-    mv "$TARGET_BIN" "$DIST_DIR/$TARGET_BIN"
+    if [[ "$ARCH" != "mips64el" ]]; then
+        cp -ar "$TARGET_BIN" "$TARGET_DIR/$TARGET_BIN.stripped"
+        upx "$TARGET_BIN"
+    fi
+    mv "$TARGET_BIN" "$TARGET_DIR/$TARGET_BIN"
 }
 
 main "$@"
