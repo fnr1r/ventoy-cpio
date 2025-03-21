553d1ec1cfe4839890d5a454d0ffdd2b21409199
fnrir <stepien241@gmail.com>
2025-03-21 11:14:51 +0100

    feat: add vtchmod

--- scripts/build_arch.sh
+++ scripts/build_arch.sh
@@ -9,6 +9,33 @@ ARCH="$2"
 
 XZ_FLAGS=(-c -e -9)
 
+copy_with() {
+    local arch="$1" suffix="$2"
+    xz "${XZ_FLAGS[@]}" \
+        "$REPO_DIR/tools/busybox/dist/${arch}_busybox" \
+        > "busybox${suffix}.xz"
+    cp -a \
+        "$REPO_DIR/tools/vtchmod/dist/${arch}/vtchmod" \
+        "vtchmod${suffix}"
+    cp -a \
+        "$REPO_DIR/tools/xz-embedded/dist/${arch}/xzminidec" \
+        "xzminidec${suffix}"
+}
+
+prepare_files_arm64() {
+    cp -aL \
+        "$REPO_DIR/tools/busybox/dist/aarch64_ash" \
+        a64
+    copy_with aarch64 aa64
+}
+
+prepare_files_mips64el() {
+    cp -aL \
+        "$REPO_DIR/tools/busybox/dist/x86_64_ash" \
+        ash
+    copy_with mips64el m64e
+}
+
 prepare_files_x86() {
     cp -aL \
         "$REPO_DIR/tools/busybox/dist/x86_64_ash" \
@@ -16,18 +43,8 @@ prepare_files_x86() {
     cp -aL \
         "$REPO_DIR/tools/busybox/dist/i386_ash" \
         ash
-    xz "${XZ_FLAGS[@]}" \
-        "$REPO_DIR/tools/busybox/dist/i386_busybox" \
-        > busybox32.xz
-    xz "${XZ_FLAGS[@]}" \
-        "$REPO_DIR/tools/busybox/dist/x86_64_busybox" \
-        > busybox64.xz
-    cp -a \
-        "$REPO_DIR/tools/xz-embedded/dist/i386/xzminidec" \
-        xzminidec32
-    cp -a \
-        "$REPO_DIR/tools/xz-embedded/dist/x86_64/xzminidec" \
-        xzminidec64
+    copy_with x86_64 64
+    copy_with i386 32
 }
 
 prepare_files() {
@@ -39,7 +56,7 @@ prepare_files() {
     popd > /dev/null
     xz "${XZ_FLAGS[@]}" \
         "$REPO_DIR/build/tool_${ARCH}.cpio" \
-        > "tool_${ARCH}.cpio.xz"
+        > "tool.cpio.xz"
     popd > /dev/null
 }
 
--- scripts/build_arch_tool.sh
+++ scripts/build_arch_tool.sh
@@ -10,6 +10,23 @@ ARCH="$2"
 
 #XZ_FLAGS=(-c -e -9)
 
+copy_with() {
+    local arch="$1" suffix="$2"
+    # TODO
+}
+
+prepare_files_arm64() {
+    cp -a \
+        "$REPO_DIR/tools/device-mapper/dist/aarch64/dmsetup" \
+        dmsetupaa64
+    cp -a \
+        "$REPO_DIR/tools/squashfs/dist/aarch64/unsquashfs" \
+        unsquashfs64
+    cp -a \
+        "$REPO_DIR/tools/zstd/dist/aarch64/zstdcat" \
+        zstdcataa64
+}
+
 prepare_files_x86() {
     cp -a \
         "$REPO_DIR/tools/device-mapper/dist/i386/dmsetup" \
@@ -17,6 +34,12 @@ prepare_files_x86() {
     cp -a \
         "$REPO_DIR/tools/device-mapper/dist/x86_64/dmsetup" \
         dmsetup64
+    cp -a \
+        "$REPO_DIR/tools/squashfs/dist/i386/unsquashfs" \
+        unsquashfs32
+    cp -a \
+        "$REPO_DIR/tools/squashfs/dist/x86_64/unsquashfs" \
+        unsquashfs64
     cp -a \
         "$REPO_DIR/tools/zstd/dist/i386/zstdcat" \
         zstdcat32
