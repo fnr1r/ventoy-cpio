2687e55c7622d3884077899735d93f163756d544
fnrir <stepien241@gmail.com>
2025-03-26 07:30:42 +0100

    feat: base cpio build in make

--- cpio.base.mk
+++ cpio.base.mk
@@ -4,21 +4,43 @@ include $(SCRIPTS_DIR)/shared.mk
 BUILD_DIR := build
 DIST_DIR ?= dist
 SRC_DIR := base
+WORK_DIR := $(BUILD_DIR)/ventoy
 
-BASE_CPIOS := build/hook.cpio.xz build/loop.cpio.xz
-BASE_SCRIPTS := build/ventoy_chain.sh.xz build/ventoy_loop.sh.xz
 BASE_FILES := $(SRC_DIR)/sbin/init $(wildcard $(SRC_DIR)/ventoy/init*)
+BASE_CPIO_NAMES := hook loop
+BASE_SCRIPT_NAMES := ventoy_chain ventoy_loop
+BASE_SYMLINKS := init linuxrc
 
-$(DIST_DIR)/ventoy.cpio: $(BASE_CPIOS) $(BASE_SCRIPTS) $(BASE_FILES)
-	bash scripts/build_base.sh $@
+BASE_CPIOS := $(foreach i,$(BASE_CPIO_NAMES),$(WORK_DIR)/ventoy/$i.cpio.xz)
+BASE_SCRIPTS := $(foreach i,$(BASE_SCRIPT_NAMES),$(WORK_DIR)/ventoy/$i.sh.xz)
 
-build/%.sh.xz: base/ventoy/%.sh
+TARGETS_COPIED := $(patsubst $(SRC_DIR)/%,$(WORK_DIR)/%,$(BASE_FILES))
+TARGETS_COMPRESSED := $(BASE_CPIOS) $(BASE_SCRIPTS)
+TARGETS_SYMLINKS := $(foreach i,$(BASE_SYMLINKS),$(WORK_DIR)/$i)
+TARGETS := $(TARGETS_COPIED) $(TARGETS_COMPRESSED) $(TARGETS_SYMLINKS)
+
+$(DIST_DIR)/ventoy.cpio: $(TARGETS)
+	@mkdir -p $(dir $@)
+	cd $(WORK_DIR) && \
+		find . \
+		| cpio -o -H newc --owner=root:root \
+		> $(HERE)/$@
+
+$(TARGETS_SYMLINKS):
+	@mkdir -p $(dir $@)
+	ln -s sbin/init $@
+
+$(WORK_DIR)/%.sh.xz: $(SRC_DIR)/%.sh
 	@mkdir -p $(dir $@)
 	cat $< | xz $(XZ_FLAGS) > $@
 
-build/%.cpio.xz: $(wildcard $(SRC_DIR)/ventoy/%/*)
+$(WORK_DIR)/%.cpio.xz: $(shell find $(SRC_DIR)/ventoy/$%)
 	@mkdir -p $(dir $@)
-	cd base/ventoy && \
-		find ./$(patsubst build/%.cpio.xz,%,$@) \
+	cd $< && \
+		find $(patsubst $(WORK_DIR)/ventoy/%.cpio.xz,%,$@) \
 		| cpio -o -H newc --owner=root:root \
 		| xz $(XZ_FLAGS) > $(HERE)/$@
+
+$(WORK_DIR)/%: $(SRC_DIR)/%
+	@mkdir -p $(dir $@)
+	cp -a $< $@
