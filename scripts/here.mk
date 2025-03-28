b0cd5813307ccc7981e98f43800026ae3a8ff8a2
fnrir <stepien241@gmail.com>
2025-03-28 21:12:30 +0100

    chore: don't redefine HERE

--- scripts/here.mk
+++ scripts/here.mk
@@ -1,6 +1,8 @@
-HERE := $(patsubst %/,%,$(dir $(realpath $(firstword $(MAKEFILE_LIST)))))
-
 THIS_MK := $(patsubst %/,%,$(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+ifndef HERE
+HERE := $(patsubst %/,%,$(dir $(realpath $(firstword $(MAKEFILE_LIST)))))
+endif
 
 ifndef SCRIPTS_DIR
 SCRIPTS_DIR := $(THIS_MK)
