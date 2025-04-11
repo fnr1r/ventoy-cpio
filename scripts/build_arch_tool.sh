fnr1r/ventoy-cpio

commit 771bb9fbdc33498d4ce648d143e4c83d0f3223f0

feat(mips64el): don't add bins not in official ramdisk

scripts/build_arch_tool.sh
@@ -19,19 +19,20 @@ copy_with() {
         "$REPO_DIR/tools/lz4/dist/${arch}/lz4cat" \
         "lz4cat${suffix}"
     cp -a \
+        "$REPO_DIR/tools/vtoytool/dist/${arch}/vtoytool" \
+        "vtoytool/00/vtoytool_${suffix}"
+    if [[ "$arch" != "mips64" ]]; then
+        return
+    fi
+    cp -a \
         "$REPO_DIR/tools/lunzip/dist/${arch}/lunzip" \
         "lunzip${suffix}"
     cp -a \
         "$REPO_DIR/tools/squashfs/dist/${arch}/unsquashfs" \
         "unsquashfs_${suffix}"
-    if [[ "$arch" != "mips64el" ]]; then
-        cp -a \
-            "$REPO_DIR/tools/vblade/dist/${arch}/vblade" \
-            "vblade_${suffix}"
-    fi
     cp -a \
-        "$REPO_DIR/tools/vtoytool/dist/${arch}/vtoytool" \
-        "vtoytool/00/vtoytool_${suffix}"
+        "$REPO_DIR/tools/vblade/dist/${arch}/vblade" \
+        "vblade_${suffix}"
     cp -a \
         "$REPO_DIR/tools/zstd/dist/${arch}/zstdcat" \
         "zstdcat${suffix}"
@@ -55,9 +56,9 @@ prepare_files_x86() {
 
 prepare_files_mips64() {
     copy_with mips64el m64e
-    cp -a \
-        "$REPO_DIR/tools/vtoy_fuse_iso/dist/mips64el/vtoy_fuse_iso" \
-        "vtoy_fuse_iso_m64e"
+    #cp -a \
+    #    "$REPO_DIR/tools/vtoy_fuse_iso/dist/mips64el/vtoy_fuse_iso" \
+    #    "vtoy_fuse_iso_m64e"
 }
 
 prepare_files() {

commit 6aa78e2495bc3d63f137cd057232a4e231f7004e

fix: condition in build_arch_tool.sh bug introduced in 771bb9fbdc33498d4ce648d143e4c83d0f3223f0 I'm idot

scripts/build_arch_tool.sh
@@ -21,7 +21,7 @@ copy_with() {
     cp -a \
         "$REPO_DIR/tools/vtoytool/dist/${arch}/vtoytool" \
         "vtoytool/00/vtoytool_${suffix}"
-    if [[ "$arch" != "mips64" ]]; then
+    if [[ "$arch" == "mips64el" ]]; then
         return
     fi
     cp -a \