e55c111fa75752e262e48bcde9e4cb4e1655bada
fnrir <fnr1r0@protonmail.com>
2025-06-03 15:48:23 +0200

    feat: add repro flag to base cpio build

--- cpio.base.mk
+++ cpio.base.mk
@@ -1,46 +1,68 @@
 include scripts/here.mk
 include $(SCRIPTS_DIR)/shared.mk
 
+ifneq ($(REPRODUCIBLE),)
+RSCPIO_STRIP := | rscpio strip --reset-ino --reset-mtime --sort
+else
+RSCPIO_STRIP :=
+endif
+
+CPIO_FLAGS := -H newc --owner=root:root
+
 BUILD_DIR := build
-DIST_DIR ?= dist
+DIST_DIR := dist
 SRC_DIR := base
 WORK_DIR := $(BUILD_DIR)/ventoy
+WORK_DIR_BASE := $(WORK_DIR)/base
 
 BASE_FILES := $(SRC_DIR)/sbin/init $(wildcard $(SRC_DIR)/ventoy/init*)
 BASE_CPIO_NAMES := hook loop
 BASE_SCRIPT_NAMES := ventoy_chain ventoy_loop
 BASE_SYMLINKS := init linuxrc
 
-BASE_CPIOS := $(foreach i,$(BASE_CPIO_NAMES),$(WORK_DIR)/ventoy/$i.cpio.xz)
-BASE_SCRIPTS := $(foreach i,$(BASE_SCRIPT_NAMES),$(WORK_DIR)/ventoy/$i.sh.xz)
+BASE_CPIOS := $(foreach i,$(BASE_CPIO_NAMES),$(WORK_DIR_BASE)/ventoy/$i.cpio.xz)
+BASE_SCRIPTS := $(foreach i,$(BASE_SCRIPT_NAMES),$(WORK_DIR_BASE)/ventoy/$i.sh.xz)
 
-TARGETS_COPIED := $(patsubst $(SRC_DIR)/%,$(WORK_DIR)/%,$(BASE_FILES))
+TARGETS_COPIED := $(patsubst $(SRC_DIR)/%,$(WORK_DIR_BASE)/%,$(BASE_FILES))
 TARGETS_COMPRESSED := $(BASE_CPIOS) $(BASE_SCRIPTS)
-TARGETS_SYMLINKS := $(foreach i,$(BASE_SYMLINKS),$(WORK_DIR)/$i)
-TARGETS := $(TARGETS_COPIED) $(TARGETS_COMPRESSED) $(TARGETS_SYMLINKS)
+TARGETS_SYMLINKS := $(addprefix $(WORK_DIR_BASE)/,$(BASE_SYMLINKS))
+BASE_TARGETS := $(TARGETS_COPIED) $(TARGETS_COMPRESSED) $(TARGETS_SYMLINKS)
 
-$(DIST_DIR)/ventoy.cpio: $(TARGETS)
+$(DIST_DIR)/ventoy.cpio: $(BASE_TARGETS)
 	@mkdir -p $(dir $@)
-	cd $(WORK_DIR) && \
+	cd $(WORK_DIR_BASE) && \
 		find . \
-		| cpio -o -H newc --owner=root:root \
+		| cpio -o $(CPIO_FLAGS) \
+		$(RSCPIO_STRIP) \
 		> $(HERE)/$@
+
+$(WORK_DIR_BASE)/ventoy/%.xz: $(WORK_DIR)/%.xz
+	@mkdir -p $(dir $@)
+	cp -a $< $@
 
 $(TARGETS_SYMLINKS):
 	@mkdir -p $(dir $@)
-	ln -s sbin/init $@
+	ln -sf sbin/init $@
 
-$(WORK_DIR)/%.sh.xz: $(SRC_DIR)/%.sh
+$(WORK_DIR)/%.sh.xz: $(SRC_DIR)/ventoy/%.sh
 	@mkdir -p $(dir $@)
 	cat $< | xz $(XZ_FLAGS) > $@
 
-$(WORK_DIR)/%.cpio.xz: $(shell find $(SRC_DIR)/ventoy/$%)
-	@mkdir -p $(dir $@)
-	cd $< && \
-		find $(patsubst $(WORK_DIR)/ventoy/%.cpio.xz,%,$@) \
-		| cpio -o -H newc --owner=root:root \
-		| xz $(XZ_FLAGS) > $(HERE)/$@
+define cpiodef
+$(eval
+$(WORK_DIR)/$1.cpio.xz: $$(shell find $(SRC_DIR)/ventoy/$1)
+	mkdir -p $(WORK_DIR)/$1
+	cp -a $(SRC_DIR)/ventoy/$1/. $(WORK_DIR)/$1
+	cd $(WORK_DIR) && \
+		find $1 \
+		| cpio -o $(CPIO_FLAGS) \
+		$(RSCPIO_STRIP) \
+		| xz $(XZ_FLAGS) > $(HERE)/$$@
+)
+endef
 
-$(WORK_DIR)/%: $(SRC_DIR)/%
+$(foreach c,$(BASE_CPIO_NAMES),$(call cpiodef,$c))
+
+$(WORK_DIR)/base/%: $(SRC_DIR)/%
 	@mkdir -p $(dir $@)
 	cp -a $< $@
